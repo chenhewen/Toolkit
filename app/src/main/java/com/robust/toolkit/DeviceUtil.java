866c1e36deae0a48074595635e5768c7f24647dc
chenhewen <532531210@qq.com>
2016-06-14 00:44:49 +0800

    DeviceUtil

--- app/src/main/java/com/robust/toolkit/DeviceUtil.java
+++ app/src/main/java/com/robust/toolkit/DeviceUtil.java
@@ -1,13 +1,20 @@
 package com.robust.toolkit;
 
+import android.content.Context;
+import android.graphics.Point;
+import android.graphics.PointF;
 import android.os.Build;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.WindowManager;
 
 import java.util.Iterator;
 import java.util.LinkedHashMap;
+import java.util.Locale;
 import java.util.Map;
-import java.util.TreeMap;
 
 /**
+ * 设备辅助工具类
  * Created by Administrator on 2016/6/9.
  */
 public class DeviceUtil {
@@ -62,7 +69,7 @@ public class DeviceUtil {
 
     public static String getBasicInfo() {
 
-        StringBuffer sb = new StringBuffer();
+        StringBuilder sb = new StringBuilder();
 
         Iterator<Map.Entry<String, String>> iterator = sMap.entrySet().iterator();
         while (iterator.hasNext()) {
@@ -73,5 +80,109 @@ public class DeviceUtil {
         return sb.toString();
     }
 
-    //TODO：内存， CPU， 屏幕信息
+    //TODO：内存，存储空间， CPU， 屏幕信息
+
+    /**
+     * 获取屏幕宽高，单位px， 会跟随屏幕旋转改变
+     * @param context context
+     * @return point
+     */
+    public static Point getScreenSizeInPx(Context context) {
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+        Point p = new Point();
+        display.getRealSize(p);
+
+        return p;
+    }
+
+    /**
+     * 获取density， 该值是厂商定的， 一般为 1， 2， 3， 4
+     * @param context context
+     * @return value
+     */
+    public static float getDensity(Context context) {
+        DisplayMetrics metrics = context.getResources().getDisplayMetrics();
+        return metrics.density;
+    }
+
+    /**
+     * 获取densityDpi, 其值为 density * 160
+     * @param context context
+     * @return value
+     */
+    public static int getDensityDpi(Context context) {
+        DisplayMetrics metrics = context.getResources().getDisplayMetrics();
+        return metrics.densityDpi;
+    }
+
+    /**
+     * 理论上该手机应该从哪一个资源文件中读取资源
+     * @param context context
+     * @return string
+     */
+    public static String getResourcesFrom(Context context) {
+        float density = getDensity(context);
+        if (density <= 0.75) {
+            return "l";
+        } else if (density <= 1) {
+            return "m";
+        } else if (density <= 1.5) {
+            return "h";
+        } else if (density <= 2) {
+            return "xh";
+        } else if (density <= 3) {
+            return "xxh";
+        } else if (density <= 4) {
+            return "xxxh";
+        } else {
+            return "unknown";
+        }
+    }
+
+    /**
+     * 获取屏幕宽高，单位dp， 会跟随屏幕旋转改变
+     * @param context context
+     * @return point
+     */
+    public static Point getScreenSizeInDp(Context context) {
+        Point point = getScreenSizeInPx(context);
+        float density = getDensity(context);
+        return new Point((int) (point.x / density), (int) (point.y / density));
+    }
+
+    /**
+     * 获取手机宽高dpi
+     * @param context context
+     * @return point
+     */
+    public static PointF getPhysicalDpi(Context context) {
+        DisplayMetrics metrics = context.getResources().getDisplayMetrics();
+        PointF p = new PointF(metrics.xdpi, metrics.ydpi);
+        return p;
+    }
+
+    /**
+     * 获取手机宽高，单位英寸
+     * @param context context
+     * @return pointF
+     */
+    public static PointF getScreenSizeInInCm(Context context) {
+        PointF pointDpi = getPhysicalDpi(context);
+        Point pointSize = getScreenSizeInPx(context);
+
+        return new PointF(pointSize.x / pointDpi.x, pointSize.y / pointDpi.y);
+    }
+
+    /**
+     * 获取手机对角线长度，单位英寸
+     * @param context context
+     * @return string
+     */
+    public static String getScreenHypotInInCm(Context context) {
+        PointF point = getScreenSizeInInCm(context);
+        double hypot = Math.hypot(point.x, point.y);
+        return String.format(Locale.getDefault(), "%.1f", hypot);
+    }
+
 }
--- app/src/main/java/com/robust/toolkit/MainActivity.java
+++ app/src/main/java/com/robust/toolkit/MainActivity.java
@@ -1,6 +1,8 @@
 package com.robust.toolkit;
 
 import android.app.Activity;
+import android.graphics.Point;
+import android.graphics.PointF;
 import android.os.Bundle;
 
 import com.orhanobut.logger.Logger;
@@ -15,6 +17,27 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Logger.t(TAG).d(DeviceUtil.getBasicInfo());
+
+        Point screenSizeInPx = DeviceUtil.getScreenSizeInPx(this);
+        float density = DeviceUtil.getDensity(this);
+        int densityDpi = DeviceUtil.getDensityDpi(this);
+        String resourcesFrom = DeviceUtil.getResourcesFrom(this);
+        Point screenSizeInDp = DeviceUtil.getScreenSizeInDp(this);
+        PointF physicalDpi = DeviceUtil.getPhysicalDpi(this);
+        PointF screenSizeInInCm = DeviceUtil.getScreenSizeInInCm(this);
+        String screenHypotInInCm = DeviceUtil.getScreenHypotInInCm(this);
+
+
+        Logger.t(TAG).d("screenSizeInPx width = %s, screenSizeInPx height = %s \n" +
+                        "density = %s, densityDpi = %s, readResourcesFrom = %s \n" +
+                        "screenSizeInDp width = %s, screenSizeInDp height = %s \n" +
+                        "physicalDpi xDpi = %s, physicalDpi yDpi = %s \n" +
+                        "screenSizeInInCm width = %.1f, screenSizeInInCm height = %.1f, screenHypotInInCm = %s",
+                screenSizeInPx.x, screenSizeInPx.y,
+                density, densityDpi, resourcesFrom,
+                screenSizeInDp.x, screenSizeInDp.y,
+                physicalDpi.x, physicalDpi.y,
+                screenSizeInInCm.x, screenSizeInInCm.y, screenHypotInInCm
+        );
     }
 }
