a64a4aa4d62a747b61839a9d7ef95d28fd7a0d53
chenhewen <532531210@qq.com>
2016-06-27 18:53:25 +0800

    内容 actionBar 全部代码

--- app/src/main/java/com/robust/toolkit/ActionBarActivity.java
+++ app/src/main/java/com/robust/toolkit/ActionBarActivity.java
@@ -1,14 +1,14 @@
 package com.robust.toolkit;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.ShareActionProvider;
 
 /**
@@ -25,6 +25,9 @@ public class ActionBarActivity extends AppCompatActivity {
         setContentView(R.layout.action_bar_activity_content_layout);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
+        ActionBar actionBar = getSupportActionBar();
+        //返回上一级, 同时Manifest要进行相应设置
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
@@ -54,5 +57,11 @@ public class ActionBarActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
 
         }
+    }
+
+    public static Intent getEntryIntent(Context context, String entry) {
+        Intent intent = new Intent();
+        intent.setClass(context.getApplicationContext(), ActionBarActivity.class);
+        return intent;
     }
 }
--- app/src/main/java/com/robust/toolkit/MainActivity.java
+++ app/src/main/java/com/robust/toolkit/MainActivity.java
@@ -1,8 +1,10 @@
 package com.robust.toolkit;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -13,6 +15,13 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity_content_layout);
-        mCardView = (CardView) findViewById(R.id.card_view);
+        View image = findViewById(R.id.image);
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent entryIntent = ActionBarActivity.getEntryIntent(MainActivity.this, null);
+                startActivity(entryIntent);
+            }
+        });
     }
 }
