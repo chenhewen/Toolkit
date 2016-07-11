6d5f5bc8b3d3c1fb8fdc3f3bc59263407ac71f0b
chenhewen <532531210@qq.com>
2016-07-11 16:39:37 +0800

    内容 animation

---
+++ app/src/main/java/com/robust/toolkit/AnimationActivity.java
@@ -0,0 +1,59 @@
+package com.robust.toolkit;
+
+import android.animation.TimeInterpolator;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.LayoutAnimationController;
+import android.view.animation.ScaleAnimation;
+
+import com.orhanobut.logger.Logger;
+
+/**
+ * Created by chenhewen on 16-7-11.
+ */
+public class AnimationActivity extends Activity {
+
+    public static final String TAG = "AnimationActivity";
+
+    int mLength = 100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.animation_activity_content_latout);
+        final ViewGroup contentLayout = (ViewGroup) findViewById(R.id.content_layout);
+
+        Animation animation = new ScaleAnimation(0, 1, 0, 1);
+        animation.setDuration(1000);
+        final LayoutAnimationController controller = new LayoutAnimationController(animation);
+
+        contentLayout.setLayoutAnimation(controller);
+        contentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //如果视图没刷新, 那么这句话就没什么用
+                contentLayout.startLayoutAnimation();
+                contentLayout.setTranslationX(mLength += 100);
+                logLocation(contentLayout);
+            }
+        });
+        contentLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                logLocation(contentLayout);
+            }
+        });
+    }
+
+    private void logLocation(View v) {
+        Logger.t(TAG).d("getLeft = %s, getTranslationX = %s, getX = %s",
+                v.getLeft(),
+                v.getTranslationX(),
+                v.getX());
+    }
+
+
+}
---
+++ app/src/main/java/com/robust/toolkit/ViewPropertyAnimatorActivity.java
@@ -0,0 +1,68 @@
+package com.robust.toolkit;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+/**
+ * Created by chenhewen on 16-7-11.
+ */
+public class ViewPropertyAnimatorActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view_property_animator_activity_content_layout);
+
+        final LinearLayout container = (LinearLayout) findViewById(R.id.container);
+
+        final Button fadeOut = (Button) findViewById(R.id.fadeOut);
+        final Button fadeIn = (Button) findViewById(R.id.fadeIn);
+        final Button moveOver = (Button) findViewById(R.id.moveOver);
+        final Button moveBack = (Button) findViewById(R.id.moveBack);
+        final Button rotate = (Button) findViewById(R.id.rotate);
+        final TextView animatingButton = (TextView) findViewById(R.id.animatingButton);
+
+        // Set long default duration for the animator, for the purposes of this demo
+        animatingButton.animate().setDuration(2000);
+
+        fadeOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animatingButton.animate().alpha(0);
+            }
+        });
+
+        fadeIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animatingButton.animate().alpha(1);
+            }
+        });
+
+        moveOver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int xValue = container.getWidth() - animatingButton.getWidth();
+                int yValue = container.getHeight() - animatingButton.getHeight();
+                animatingButton.animate().x(xValue).y(yValue);
+            }
+        });
+
+        moveBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animatingButton.animate().x(0).y(0);
+            }
+        });
+
+        rotate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animatingButton.animate().rotationXBy(720);
+            }
+        });
+    }
+}
