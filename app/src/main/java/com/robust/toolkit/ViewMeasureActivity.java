f33f2861cda95f9200d62a4d85ebdb003d7981e7
chenhewen <532531210@qq.com>
2016-07-30 17:39:44 +0800

    ViewRootActivity

---
+++ app/src/main/java/com/robust/toolkit/ViewMeasureActivity.java
@@ -0,0 +1,16 @@
+package com.robust.toolkit;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by chenhewen on 16-7-21.
+ */
+public class ViewMeasureActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view_measure_activity_content_layout);
+    }
+}
